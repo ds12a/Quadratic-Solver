842cb0ac267c38af1c35a3ae1a8ad754f021041d
ds12a <81164854+ds12a@users.noreply.github.com>
2021-10-28 17:40:39 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ var bInput = document.getElementById('b');
 var cInput = document.getElementById('c');
 
 function buildCalculations(r1, r2, numSolutions) {
-  var solutionInfo = '<div id="calculations"><p>';
+  var solutionInfo = '<div id="calculations"> <p>';
   if (numSolutions == 0) solutionInfo = "NO ROOTS";
   else if (numSolutions == 1) solutionInfo = "One root:</p><p>x = " + toString(r1);
   else if (numSolutions == 2) solutionInfo = "Two roots:</p><p>x = " + toString(r1) + "</p>" + "<p> x = "+ toString(r1);
-  solutionInfo += "</p></div>";
+  solutionInfo += "</p> </div>";
   
   return solutionInfo;
 }
@@ -31,6 +31,7 @@ function updateCalculations() {
   var r2 = 2;
   
   var out = document.getElementById('calculations');
+  alert(out.outerHTML);
   out.outerHTML = buildCalculations(r1, r2, 2);
   
   alert(out.outerHTML);
